db4fd532d233154fdec3d5848278a6539adf5388
pudep <261843105+pudep@users.noreply.github.com>
2026-08-04 21:11:35 +0530

    feat: initialize main.rs with basic code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-  println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "pp")]
+struct Cli {
+  #[command(subcommand)]
+  command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  List,
+  Show,
+  Kill,
+  Stop,
+  Run {
+    // Get each single quote string as a String into the args vector.
+    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+    args: Vec<String>,
+  },
+}
+
+fn string_lexer(str: &str) -> Vec<String> {
+  shlex::split(str).unwrap_or_default()
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+  let cli = Cli::parse();
+  match &cli.command {
+    Commands::Run { args } => {
+      // Get the string item.
+      for item in args.iter() {
+        // lex it
+        let argv = string_lexer(item.as_str());
+
+        if argv.is_empty() {
+          continue;
+        }
+
+        // summon a process
+        std::process::Command::new(&argv[0])
+          .args(&argv[1..])
+          .status()?;
+      }
+    }
+    _ => {
+      println!("None")
+    }
+  }
+  Ok(())
 }
