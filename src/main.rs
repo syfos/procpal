6ba4f4f75a9c6763df245410e54cc69d50d5983c
pudep <261843105+pudep@users.noreply.github.com>
2026-08-05 09:45:34 +0530

    refactor: make main.rs minimal

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,12 @@
-use clap::{Parser, Subcommand};
+use crate::{cmd_parser::Cli, string_lexer::App};
+use clap::Parser;
 
-#[derive(Parser)]
-#[command(name = "pp")]
-struct Cli {
-  #[command(subcommand)]
-  command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-  List,
-  Show,
-  Kill,
-  Stop,
-  Run {
-    // Get each single quote string as a String into the args vector.
-    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
-    args: Vec<String>,
-  },
-}
-
-fn string_lexer(str: &str) -> Vec<String> {
-  shlex::split(str).unwrap_or_default()
-}
+pub mod cmd_parser;
+pub mod string_lexer;
+pub mod errors;
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
   let cli = Cli::parse();
-  match &cli.command {
-    Commands::Run { args } => {
-      // Get the string item.
-      for item in args.iter() {
-        // lex it
-        let argv = string_lexer(item.as_str());
-
-        if argv.is_empty() {
-          continue;
-        }
-
-        // summon a process
-        std::process::Command::new(&argv[0])
-          .args(&argv[1..])
-          .status()?;
-      }
-    }
-    _ => {
-      println!("None")
-    }
-  }
+  App::argv_parser(&cli)?;
   Ok(())
 }
